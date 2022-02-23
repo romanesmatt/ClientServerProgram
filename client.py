10a4df3f1d74f85cf741717b018c273b8adfc67a
Matt Romanes <48772682+romanesmatt@users.noreply.github.com>
2022-02-24 12:43:36 +1300

    Add client file
    In addition, implement communication between client and server

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+
+# Creating the socket
+s = socket.socket()
+print("Socket successfully created.")
+
+# Reserving a port on machine
+port = 3004
+
+s.connect(("127.0.0.1", port))
+
+# Receiving data from the server and decoding it to get the string
+print(s.recv(1024).decode())
+
+# Closing the connection
+s.close()
+print("Connection closed")
